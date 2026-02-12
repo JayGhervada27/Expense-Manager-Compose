067cc7320c0d76c10619704a487c19c0602d4e8c
Jay Ghervada <innovationsolution2k23@gmail.com>
2026-02-12 21:59:12 +0530

    - Add Spacing

--- app/src/main/java/com/example/expensemanager/MainActivity.kt
+++ app/src/main/java/com/example/expensemanager/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+
         setContent {
             ExpenseManagerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
